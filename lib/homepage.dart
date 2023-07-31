f3f04bc154fc4352de47c4471327124636dc1033
zahid-eng <zali68500@gmail.com>
2023-07-31 20:38:01 +0500

    faceAnd Pose

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,247 +1,74 @@
 import 'package:camera/camera.dart';
 import 'package:downgradeface/Utils/FaceDetectorPainter.dart';
-import 'package:downgradeface/Utils/UtilsScanner.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'detector_view.dart';
 
-class HomePage extends StatefulWidget {
+class FaceDetectorView extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  State<FaceDetectorView> createState() => _FaceDetectorViewState();
 }
 
-class _HomePageState extends State<HomePage> {
-  final ValueNotifier<CameraImage?> _imageNotifier = ValueNotifier(null);
-  CameraController? _cameraController;
-  late CameraDescription _cameraDescription;
-  CameraLensDirection _cameraLensDirection = CameraLensDirection.back;
-  FaceDetector? _faceDetector;
-  bool _isWorking = false;
-  List<Face> _facesList = [];
-  void _initializeCamera() async {
-    final cameras = await availableCameras();
-    _cameraDescription = cameras[0];
-    _cameraController = CameraController(
-      _cameraDescription,
-      ResolutionPreset.medium,
-      imageFormatGroup: ImageFormatGroup.yuv420,
-    );
-    _faceDetector = FirebaseVision.instance.faceDetector(
-      FaceDetectorOptions(
-        enableClassification: true,
-        minFaceSize: 0.1,
-        mode: FaceDetectorMode.fast,
-      ),
-    );
-    try {
-      await _cameraController!.initialize();
-    } catch (e) {
-      print('Error initializing camera: $e');
-    }
-    if (!mounted) {
-      return;
-    }
-    _cameraController!.startImageStream((imageFromStream) {
-      if (!_isWorking) {
-        _isWorking = true;
-        // implement FaceDetection
-        performDetectionOnStreamFrame(imageFromStream);
-      }
-    });
-    setState(() {});
-  }
-
-  void performDetectionOnStreamFrame(CameraImage? imageFromStream) async {
-    if (imageFromStream == null || _faceDetector == null) return;
-    final FirebaseVisionImageMetadata metadata = FirebaseVisionImageMetadata(
-      rawFormat: imageFromStream.format.raw,
-      size: Size(
-        imageFromStream.width.toDouble(),
-        imageFromStream.height.toDouble(),
-      ),
-      rotation: ImageRotation.rotation90,
-    );
-    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromBytes(
-      imageFromStream.planes[0].bytes,
-      metadata,
-    );
-    try {
-      List<Face> faces = await _faceDetector!.processImage(visionImage);
-      setState(() {
-        _facesList = faces;
-      });
-    } catch (e) {
-      print('Error detecting faces: $e');
-    }
-    _isWorking = false;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _initializeCamera();
-  }
-
+class _FaceDetectorViewState extends State<FaceDetectorView> {
+  final FaceDetector _faceDetector = FaceDetector(
+    options: FaceDetectorOptions(
+      enableContours: true,
+      enableLandmarks: true,
+    ),
+  );
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  String? _text;
+  var _cameraLensDirection = CameraLensDirection.front;
   @override
   void dispose() {
+    _canProcess = false;
+    _faceDetector.close();
     super.dispose();
-    _cameraController?.dispose();
-    _faceDetector?.close();
-    _imageNotifier.dispose();
-  }
-
-  void toggleCamera() async {
-    if (_cameraLensDirection == CameraLensDirection.front) {
-      _cameraLensDirection = CameraLensDirection.back;
-    } else {
-      _cameraLensDirection = CameraLensDirection.front;
-    }
-    await _cameraController!.stopImageStream();
-    await _cameraController!.dispose();
-    final cameras = await availableCameras();
-    _cameraDescription = cameras.firstWhere(
-      (camera) => camera.lensDirection == _cameraLensDirection,
-    );
-    _cameraController = CameraController(
-      _cameraDescription,
-      ResolutionPreset.medium,
-      imageFormatGroup: ImageFormatGroup.yuv420,
-    );
-    try {
-      await _cameraController!.initialize();
-    } catch (e) {
-      print('Error initializing camera: $e');
-    }
-    _cameraController!.startImageStream((imageFromStream) {
-      if (!_isWorking) {
-        _isWorking = true;
-        // implement FaceDetection
-        performDetectionOnStreamFrame(imageFromStream);
-      }
-    });
-    setState(() {});
-  }
-
-  Widget buildResult() {
-    if (_imageNotifier.value == null ||
-        !_cameraController!.value.isInitialized) {
-      return Container();
-    }
-    final Size imageSize = Size(
-      _cameraController!.value.previewSize!.height,
-      _cameraController!.value.previewSize!.width,
-    );
-    // customPainter
-    CustomPainter customPainter = FaceDetectorPainter(
-      imageSize,
-      _imageNotifier.value! as List<Face>,
-      _cameraLensDirection,
-    );
-    return CustomPaint(
-      painter: customPainter,
-    );
   }
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    List<Widget> stackWidgetChildren = [];
-    // ... Other widget code ...
-    stackWidgetChildren.add(
-      Positioned(
-        top: 30,
-        left: 0.0,
-        width: size.width,
-        height: size.height - 250,
-        child: _facesList.isEmpty
-            ? Container(
-                color: Colors.amber,
-              )
-            : CustomPaint(
-                painter: FaceDetectorPainter(
-                  Size(
-                    _cameraController!.value.previewSize!.height,
-                    _cameraController!.value.previewSize!.width,
-                  ),
-                  _facesList,
-                  _cameraLensDirection,
-                ),
-              ),
-      ),
+    return DetectorView(
+      title: 'Face Detector',
+      customPaint: _customPaint,
+      text: _text,
+      onImage: _processImage,
+      initialCameraLensDirection: _cameraLensDirection,
+      onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
     );
-    return Scaffold(
-      body: ValueListenableBuilder<CameraImage?>(
-        valueListenable: _imageNotifier,
-        builder: (context, value, _) {
-          final List<Widget> stackWidgetChildren = [];
-          final size = MediaQuery.of(context).size;
-          // add streaming camera
-          if (_cameraController != null) {
-            stackWidgetChildren.add(
-              Positioned(
-                top: 30,
-                left: 0,
-                width: size.width,
-                height: size.height - 250,
-                child: Container(
-                  child: (_cameraController!.value.isInitialized)
-                      ? AspectRatio(
-                          aspectRatio: _cameraController!.value.aspectRatio,
-                          child: RepaintBoundary(
-                            child: CameraPreview(_cameraController!),
-                          ),
-                        )
-                      : Container(),
-                ),
-              ),
-            );
-          }
-          // toggle camera
-          stackWidgetChildren.add(
-            Positioned(
-              top: size.height - 250,
-              left: 0,
-              width: size.width,
-              height: 250,
-              child: Container(
-                margin: EdgeInsets.only(bottom: 80),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.switch_camera,
-                        color: Colors.white,
-                      ),
-                      iconSize: 50,
-                      color: Colors.black,
-                      onPressed: () {
-                        toggleCamera();
-                      },
-                    )
-                  ],
-                ),
-              ),
-            ),
-          );
-          stackWidgetChildren.add(
-            Positioned(
-              top: 30,
-              left: 0.0,
-              width: size.width,
-              height: size.height - 250,
-              child: buildResult(),
-            ),
-          );
-          return Container(
-            margin: EdgeInsets.only(top: 0),
-            color: Colors.black,
-            child: Stack(
-              children: stackWidgetChildren,
-            ),
-          );
-        },
-      ),
-    );
+  }
+
+  Future<void> _processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+    final faces = await _faceDetector.processImage(inputImage);
+    if (inputImage.metadata?.size != null &&
+        inputImage.metadata?.rotation != null) {
+      final painter = FaceDetectorPainter(
+        faces,
+        inputImage.metadata!.size,
+        inputImage.metadata!.rotation,
+        _cameraLensDirection,
+      );
+      _customPaint = CustomPaint(painter: painter);
+    } else {
+      String text = 'Faces found: ${faces.length}\n\n';
+      for (final face in faces) {
+        text += 'face: ${face.boundingBox}\n\n';
+      }
+      _text = text;
+      // TODO: set _customPaint to draw boundingRect on top of image
+      _customPaint = null;
+    }
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:camera/camera.dart';
-import 'package:downgradeface/Utils/splashScreen.dart';
+
 import 'package:downgradeface/homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SplashScreenPage(),
+      home: FaceDetectorView(),
     );
   }
 }
