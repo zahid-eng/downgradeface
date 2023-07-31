a3ed50d6d41993d799500e0982d96dd8945f8491
zahid-eng <zali68500@gmail.com>
2023-07-31 20:38:15 +0500

    commit

--- lib/Utils/FaceDetectorPainter.dart
+++ lib/Utils/FaceDetectorPainter.dart
@@ -1,42 +1,132 @@
+import 'dart:math';
+
 import 'package:camera/camera.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 
 class FaceDetectorPainter extends CustomPainter {
-  final Size absulteImageSize;
-  final List<Face> faces;
-  CameraLensDirection cameraLensDirection;
-
   FaceDetectorPainter(
-      this.absulteImageSize, this.faces, this.cameraLensDirection);
+    this.faces,
+    this.imageSize,
+    this.rotation,
+    this.cameraLensDirection,
+  );
+
+  final List<Face> faces;
+  final Size imageSize;
+  final InputImageRotation rotation;
+  final CameraLensDirection cameraLensDirection;
 
   @override
   void paint(Canvas canvas, Size size) {
-    final double scaleX = size.width / absulteImageSize.width;
-    final double scaleY = size.height / absulteImageSize.height;
-    final Paint paint = Paint()
+    final Paint paint1 = Paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 2.0
-      ..color = Colors.greenAccent;
+      ..strokeWidth = 1.0
+      ..color = Colors.red;
+    final Paint paint2 = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 1.0
+      ..color = Colors.green;
 
-    for (Face face in faces) {
+    for (final Face face in faces) {
+      final left = translateX(
+        face.boundingBox.left,
+        size,
+        imageSize,
+        rotation,
+        cameraLensDirection,
+      );
+      final top = translateY(
+        face.boundingBox.top,
+        size,
+        imageSize,
+        rotation,
+        cameraLensDirection,
+      );
+      final right = translateX(
+        face.boundingBox.right,
+        size,
+        imageSize,
+        rotation,
+        cameraLensDirection,
+      );
+      final bottom = translateY(
+        face.boundingBox.bottom,
+        size,
+        imageSize,
+        rotation,
+        cameraLensDirection,
+      );
+
       canvas.drawRect(
-          Rect.fromLTRB(
-              cameraLensDirection == CameraLensDirection.back
-                  ? face.boundingBox.left * scaleX
-                  : (absulteImageSize.width - face.boundingBox.right) * scaleX,
-              face.boundingBox.top * scaleY,
-              cameraLensDirection == CameraLensDirection.back
-                  ? face.boundingBox.right * scaleX
-                  : (absulteImageSize.width - face.boundingBox.left) * scaleX,
-              face.boundingBox.bottom * scaleY),
-          paint);
+        Rect.fromLTRB(left, top, right, bottom),
+        paint1,
+      );
+
+      void paintContour(FaceContourType type) {
+        final contour = face.contours[type];
+        if (contour?.points != null) {
+          for (final Point point in contour!.points) {
+            canvas.drawCircle(
+                Offset(
+                  translateX(
+                    point.x.toDouble(),
+                    size,
+                    imageSize,
+                    rotation,
+                    cameraLensDirection,
+                  ),
+                  translateY(
+                    point.y.toDouble(),
+                    size,
+                    imageSize,
+                    rotation,
+                    cameraLensDirection,
+                  ),
+                ),
+                1,
+                paint1);
+          }
+        }
+      }
+
+      void paintLandmark(FaceLandmarkType type) {
+        final landmark = face.landmarks[type];
+        if (landmark?.position != null) {
+          canvas.drawCircle(
+              Offset(
+                translateX(
+                  landmark!.position.x.toDouble(),
+                  size,
+                  imageSize,
+                  rotation,
+                  cameraLensDirection,
+                ),
+                translateY(
+                  landmark.position.y.toDouble(),
+                  size,
+                  imageSize,
+                  rotation,
+                  cameraLensDirection,
+                ),
+              ),
+              2,
+              paint2);
+        }
+      }
+
+      for (final type in FaceContourType.values) {
+        paintContour(type);
+      }
+
+      for (final type in FaceLandmarkType.values) {
+        paintLandmark(type);
+      }
     }
   }
 
   @override
-  bool shouldRepaint(covariant FaceDetectorPainter oldDelegate) {
-    return oldDelegate.absulteImageSize != absulteImageSize ||
-        oldDelegate.faces != faces;
+  bool shouldRepaint(FaceDetectorPainter oldDelegate) {
+    return oldDelegate.imageSize != imageSize || oldDelegate.faces != faces;
   }
 }
