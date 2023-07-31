fbfdc582786bba60240a8754d9ea43496eb03c7c
zahid-eng <zali68500@gmail.com>
2023-07-31 18:08:41 +0500

    attendanceApp

---
+++ lib/Utils/splashScreen.dart
@@ -0,0 +1,31 @@
+import 'package:downgradeface/homepage.dart';
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:splashscreen/splashscreen.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 5,
+      navigateAfterSeconds: HomePage(),
+      title: Text(
+        '\n DETECCION DE ROSTRO',
+        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+      ),
+      image: Image.asset("assets/splash.png"),
+      photoSize: 130,
+      backgroundColor: Colors.white,
+      loaderColor: Colors.black,
+      loadingText: Text(
+        'by Wanna Code',
+        style: TextStyle(color: Colors.black, fontSize: 16.00),
+      ),
+    );
+  }
+}
